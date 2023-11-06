greenplum-db/gpdb-archive

Fix Differential recovery tablespace issue (#16195)
Issue:
1. Post differential recovery more tablespace directory is created on the target segment.

[gpadmin@cdw ~]$ gpssh -f segment_host_list -v
[WARN] Reference default values as $COORDINATOR_DATA_DIRECTORY/gpssh.conf could not be found
Using delaybeforesend 0.05 and prompt_validation_timeout 1.0
[Reset ...]
[INFO] login sdw2
[INFO] login sdw1
=> ls /tmp/mytblespace/
ls /tmp/mytblespace/
[sdw2] 4 5 6 7
[sdw1] 2 3 8 9
=> exit

[gpadmin@cdw ~]$ gpssh -f segment_host_list -v
[WARN] Reference default values as $COORDINATOR_DATA_DIRECTORY/gpssh.conf could not be found
Using delaybeforesend 0.05 and prompt_validation_timeout 1.0
[Reset ...]
[INFO] login sdw1
[INFO] login sdw2
=> ls /tmp/mytblespace/
ls /tmp/mytblespace/
[sdw1] 2 3 4 5 6 7 8 9
[sdw2] 4 5 6 7
=> exit

Steps to reproduce ( tested on demo cluster):

1. make sure that the cluster is up and running in a balance state.
2. create a tablespace to a location outside of the data directory, add some tables just to have some data.
3. check the content of tablespace location(/tmp/mytblespace/) for all host.
4. pick any of the segments and make primary down and wait until mirror gets promoted.
5. run differential recovery.
6. post recover check the content of tablespace location(/tmp/mytblespace/) for all host. you will find that source tablespace data also getting populated in the target data dir.

RCA: In differential recovery when we perform the step of sync_tablespaces() there is a bug because of that we are copying all the directories under the tablespace location.
which was not intended as the get_segment_tablespace returning only base tablespace location, not the segment-specific target link location.

Fix: Updated algorithm to copy tablespaces data directory and updating the symlink. following are the steps
1. First get the tablespace location using get_segment_tablespace_oid_locations()
2. clean all the symlink files a

gpMgmt/bin/gppylib/commands/unix.py
@@ -167,6 +167,20 @@ def terminate_proc_tree(pid, sig=signal.SIGTERM, include_parent=True, timeout=No
         process.kill()
 
 
+def get_remote_link_path(path, host):
+    """
+      Function to get symlink target path for a given path on given host.
+      :param  path: path for which symlink has to be found
+      :param  host: host on which the given path is available
+      :return: returns symlink target path
+    """
+
+    cmdStr = """python3 -c 'import os; print(os.readlink("%s"))'""" % path
+    cmd = Command('get remote link path', cmdStr=cmdStr, ctxt=REMOTE,
+                       remoteHost=host)
+    cmd.run(validateAfter=True)
+    return cmd.get_stdout()
+
 # ---------------Platform Framework--------------------
 
 """ The following platform framework is used to handle any differences between

gpMgmt/test/behave/mgmt_utils/steps/tablespace_mgmt_utils.py
@@ -1,6 +1,6 @@
 import pipes
 import tempfile
-import time
+import os
 
 from behave import given, then
 
@@ -10,6 +10,7 @@ from gppylib.db import dbconn
 from gppylib.gparray import GpArray
 from test.behave_utils.utils import run_cmd,wait_for_database_dropped
 from gppylib.commands.base import Command, REMOTE
+from gppylib.commands.unix import get_remote_link_path
 
 class Tablespace:
     def __init__(self, name):
@@ -72,6 +73,32 @@ class Tablespace:
         if sorted(data) != sorted(self.initial_data):
             raise Exception("Tablespace data is not identically distributed. Expected:\n%r\n but found:\n%r" % (
                 sorted(self.initial_data), sorted(data)))
+
+    def verify_symlink(self, hostname=None, port=0):
+        url = dbconn.DbURL(hostname=hostname, port=port, dbname=self.dbname)
+        gparray = GpArray.initFromCatalog(url)
+        all_segments = gparray.getDbList()
+
+        # fetching oid of available user created tablespaces
+        with closing(dbconn.connect(url, unsetSearchPath=False)) as conn:
+            tblspc_oids = dbconn.query(conn, "SELECT oid FROM pg_tablespace WHERE spcname NOT IN ('pg_default', 'pg_global')").fetchall()
+
+        if not tblspc_oids:
+            return None  # no table space is present
+
+        # keeping a list to check if any of the symlink has duplicate entry
+        tblspc = []
+        for seg in all_segments:
+            for tblspc_oid in tblspc_oids:
+                symlink_path = os.path.join(seg.getSegmentTableSpaceDirectory(), str(tblspc_oid[0]))
+                target_path = get_remote_link_path(symlink_path, seg.getSegmentHostName())
+                segDbId = seg.getSegmentDbId()
+                #checking for duplicate and wrong symlink target
+                if target_path in tblspc or os.path.basename(target_path) != str(segDbId):
+                    raise Exception("tablespac has invalid/duplicate symlink for oid {0} in segment dbid {1}".\
+                        format(str(tblspc_oid[0]),str(segDbId)))
+
+                tblspc.append(target_path)
 
     def verify_for_gpexpand(self, hostname=None, port=0):
         """
@@ -190,6 +217,9 @@ def _create_tablespace_with_data(context, name):
 def impl(context):
     context.tablespaces["outerspace"].verify()
 
+@then('the tablespace has valid symlink')
+def impl(context):
+    context.tablespaces["outerspace"].verify_symlink()
 
 @then('the tablespace is valid on the standby coordinator')
 def impl(context):

shrakesh/gpdb

gpMgmt/bin/gppylib/commands/unix.py
@@ -173,6 +173,20 @@ def kill_sequence(pid):
     logandkill(pid, signal.SIGABRT)
 
 
+def get_remote_link_path(path, host):
+    """
+      Function to get symlink target path for a given path on given host.
+      :param  path: path for which symlink has to be found
+      :param  host: host on which the given path is available
+      :return: returns symlink target path
+    """
+
+    cmdStr = """python -c 'import os; print(os.readlink("%s"))'""" % path
+    cmd = Command('get remote link path', cmdStr=cmdStr, ctxt=REMOTE,
+                       remoteHost=host)
+    cmd.run(validateAfter=True)
+    return cmd.get_stdout()
+
 # ---------------Platform Framework--------------------
 
 """ The following platform framework is used to handle any differences between

gpMgmt/test/behave/mgmt_utils/steps/tablespace_mgmt_utils.py
@@ -1,6 +1,6 @@
 import pipes
 import tempfile
-import time
+import os
 
 from behave import given, then
 from pygresql import pg
@@ -9,6 +9,8 @@ from gppylib.db import dbconn
 from gppylib.gparray import GpArray
 from test.behave_utils.utils import run_cmd,wait_for_database_dropped
 from gppylib.commands.base import Command, REMOTE
+from gppylib.commands.unix import get_remote_link_path
+from contextlib import closing
 
 class Tablespace:
     def __init__(self, name):
@@ -71,6 +73,32 @@ class Tablespace:
         if sorted(data) != sorted(self.initial_data):
             raise Exception("Tablespace data is not identically distributed. Expected:\n%r\n but found:\n%r" % (
                 sorted(self.initial_data), sorted(data)))
+
+    def verify_symlink(self, hostname=None, port=0):
+        url = dbconn.DbURL(hostname=hostname, port=port, dbname=self.dbname)
+        gparray = GpArray.initFromCatalog(url)
+        all_segments = gparray.getDbList()
+
+        # fetching oid of available user created tablespaces
+        with closing(dbconn.connect(url, unsetSearchPath=False)) as conn:
+            tblspc_oids = dbconn.execSQL(conn, "SELECT oid FROM pg_tablespace WHERE spcname NOT IN ('pg_default', 'pg_global')").fetchall()
+
+        if not tblspc_oids:
+            return None  # no table space is present
+
+        # keeping a list to check if any of the symlink has duplicate entry
+        tblspc = []
+        for seg in all_segments:
+            for tblspc_oid in tblspc_oids:
+                symlink_path = os.path.join(seg.getSegmentTableSpaceDirectory(), str(tblspc_oid[0]))
+                target_path = get_remote_link_path(symlink_path, seg.getSegmentHostName())
+                segDbId = seg.getSegmentDbId()
+                #checking for duplicate and wrong symlink target
+                if target_path in tblspc or os.path.basename(target_path) != str(segDbId):
+                    raise Exception("tablespac has invalid/duplicate symlink for oid {0} in segment dbid {1}".\
+                        format(str(tblspc_oid[0]),str(segDbId)))
+
+                tblspc.append(target_path)
 
     def verify_for_gpexpand(self, hostname=None, port=0):
         """
@@ -191,6 +219,9 @@ def _create_tablespace_with_data(context, name):
 def impl(context):
     context.tablespaces["outerspace"].verify()
 
+@then('the tablespace has valid symlink')
+def impl(context):
+    context.tablespaces["outerspace"].verify_symlink()
 
 @then('the tablespace is valid on the standby master')
 def impl(context):